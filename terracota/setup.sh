07e90b7e6d97e7ca72ae48f5f13f7c350d9d66bf
Kássia Catarine <kassia_catarine.15@hotmail.com>
2017-06-18 09:59:54 -0300

    Adicionando setup.sh
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- terracota/setup.sh
+++ terracota/setup.sh
@@ -2,7 +2,7 @@ echo "Updating packages"
 sudo apt-get update
 echo "Terracota game installation"
 echo "Installation of required packages"
-sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer
+sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev
 echo "Updating packages"
 sudo apt-get update
 echo "Unpack ijengine.taz.gz"
